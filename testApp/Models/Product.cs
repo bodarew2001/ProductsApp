262fe07cb0009c16788b77118a3d6b269bbcc226
Victor Bodarev <victor.bodarev.2001@gmail.com>
2022-11-06 17:27:45 +0200

    Add project files.

---
+++ testApp/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testApp.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Orders = new HashSet<Order>();
+            Categories = new HashSet<Category>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = null!;
+        public int Stock { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+        public virtual ICollection<Category> Categories { get; set; }
+    }
+}
---
+++ testApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testApp.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
