f6a598b84cccedcc83ee6225eb8e0413de84b463
Victor Bodarev <victor.bodarev.2001@gmail.com>
2022-12-16 08:51:45 +0200

    Auto migrations

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -52,4 +52,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+using (var scope = host.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
